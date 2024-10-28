26dbf70dcd22e3219e58fdfd07da7de727bdb181
projectDiscover <wood.onlineactivity@gmail.com>
2024-10-27 20:58:13 -0500

    JIRA-ID-1: Boilerplate code, refactoring/clean up and persistence layer creation

---
+++ src/main/java/com/assignment/customer/controller/CustomerController.java
@@ -0,0 +1,4 @@
+package com.assignment.customer.controller;
+
+public class CustomerController {
+}
---
+++ src/main/java/com/assignment/customer/persistence/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.assignment.customer.persistence.repository;
+
+import com.assignment.customer.persistence.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
+}

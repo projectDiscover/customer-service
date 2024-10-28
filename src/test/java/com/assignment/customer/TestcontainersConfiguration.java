5897b78c7c14e08a4a6dc419b1add12261eecbb3
projectDiscover <wood.onlineactivity@gmail.com>
2024-10-27 20:56:54 -0500

    JIRA-ID-1: Boilerplate code, refactoring/clean up and persistence layer creation

--- src/test/java/com/assignment/customer/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.customer;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}

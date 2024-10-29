64554cf42e2db777a9a5abf268b96caebdf50d92
projectDiscover <wood.onlineactivity@gmail.com>
2024-10-28 21:40:17 -0500

    JIRA-ID-2: Swagger & Open API Spec generation.

--- src/main/java/com/assignment/customer/controller/CustomerController.java
+++ src/main/java/com/assignment/customer/controller/CustomerController.java
@@ -1,4 +1,31 @@
 package com.assignment.customer.controller;
 
+import com.assignment.customer.annotation.swagger.CreateUpdCustomerApiSwaggerDoc;
+import com.assignment.customer.model.bean.request.CustomerRequestBean;
+import com.assignment.customer.model.bean.response.CustomerResponseBean;
+import com.assignment.customer.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/customer-service/v1/")
+@Slf4j
+@RequiredArgsConstructor
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping(value = "/create-customer", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CreateUpdCustomerApiSwaggerDoc
+    public ResponseEntity<CustomerResponseBean> createUserRecord(@RequestBody @Validated final CustomerRequestBean customerRequestBean) {
+
+        return ResponseEntity.ok().body(null);
+    }
+
 }

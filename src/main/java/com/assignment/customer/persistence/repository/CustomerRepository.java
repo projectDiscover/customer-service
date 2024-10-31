f932f4fed77a88a946ba03fedadade03208a557f
projectDiscover <wood.onlineactivity@gmail.com>
2024-10-30 21:37:34 -0500

    JIRA-ID-4: Create Customer Service API impl, API Spec update, CURD endpoints creation, Persistence and service layer modification, Error handling, DB schema creation.

--- src/main/java/com/assignment/customer/persistence/repository/CustomerRepository.java
+++ src/main/java/com/assignment/customer/persistence/repository/CustomerRepository.java
@@ -3,7 +3,5 @@ package com.assignment.customer.persistence.repository;
 import com.assignment.customer.persistence.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 }
